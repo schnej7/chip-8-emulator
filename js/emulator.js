5b2bb304a609a01515230d53b845bfda846e0e67
schnej7 <schnej7@rpi.edu>
2012-08-16 19:45:24 -0400

    added switch statements to decode opcodes

--- js/emulator.js
+++ js/emulator.js
@@ -92,11 +92,159 @@ function memoryInit(){
     loadFontset();
 }
 
+function decodeAndExecute( opcode ){
+    switch( opcode & 0xF000 ){
+
+        case 0x0000:    // 0---: more decoding
+            switch( opcode & 0x000F ){
+
+                case 0x0000:    // 0x00E0: clear the screen
+                break;
+
+                case 0x000E:    // 0x00EE: return from a subroutine
+                break;
+
+                default:
+                    console.log("Unknown opcode: " + opcode.toString(16));
+            }
+        break;
+
+        case 0x1000:    // 1NNN: Jumps to address NNN
+        break
+
+        case 0x2000:    // 2NNN: Calls subroutine at address NNN
+        break;
+
+        case 0x3000:    // 3XNN: Skips the next instrouction if VX equals NN
+        break;
+
+        case 0x4000:    // 4XNN: Skips the next instrouction if VX does not equal NN
+        break;
+
+        case 0x5000:    // 5XY0: Skips the next instrouction if VX equals VY
+        break;
+
+        case 0x6000:    // 6XNN: Sets VX to NN
+        break;
+
+        case 0x7000:    // 7XNN: Adds NN to VX
+        break;
+
+        case 0x8000:    // 8---: more decoding
+            switch( opcode & 0x000F ){
+                case 0x0000:    // 8XY0: Sets VX to the value of VY
+                break;
+
+                case 0x0001:    // 8XY1: Sets VX to VX | VY
+                break;
+
+                case 0x0002:    // 8XY2: Sets VX to VX & VY
+                break;
+
+                case 0x0003:    // 8XY3: Sets VX to VX XOR VY
+                break;
+
+                case 0x0004:    // 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't
+                break;
+
+                case 0x0005:    // 8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+                break;
+
+                case 0x0006:    // 8XY6: Shifts VX right by one. VF is set to the value of the least significant bit of VX before the shift
+                break;
+
+                case 0x0007:    // 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
+                break;
+
+                case 0x000E:    // 8XYE: Shifts VX left by one. VF is set to the value of the most significant bit of VX before the shift
+                break;
+
+                default:
+                    console.log("Unknown opcode: " + opcode.toString(16));
+            }
+        break;
+
+        case 0x9000:    // Skips the next instruction if VX doesn't equal VY
+        break;
+
+        case 0xA000:    // ANNN: sets I to the address NNN
+            I = opcode & 0x0FFF;
+            pc += 2;
+        break;
+
+        case 0xB000:    // BNNN: Jumps to the address NNN plus V0
+        break;
+
+        case 0xC000:    // CXNN: Sets VX to a random number and NN
+        break;
+
+        case 0xD000:    // DXYN: Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
+                        //       Each row of 8 pixels is read as bit-coded (with the most significant bit of each byte displayed on the left) starting from memory location I;
+                        //       I value doesn't change after the execution of this instruction. 
+                        //       As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn't happen.
+        break;
+
+        case 0xE000:    // E---: more decoding
+            switch( opcode & 0x00FF ){
+
+                case 0x009E:    // EX9E: Skips the next instruction if the key stored in VX is pressed
+                break
+
+                case 0x00A1:    // EXA1: Skips the next instruction if the key stored in VX isn't pressed
+                break;
+
+                default:
+                    console.log("Unknown opcode: " + opcode.toString(16));
+            }
+        break;
+
+        case 0xF000:    // F000: more decoding
+            switch( opcode & 0x00FF ){
+
+                case 0x0007:    // FX07: Sets VX to the value of the delay timer
+                break;
+
+                case 0x000A:    // FX0A: A key press is awaited, and then stored in VX
+                break;
+
+                case 0x0015:    // FX15: Sets the delay timer to VX
+                break;
+
+                case 0x0018:    // FX19: Sets the sound timer to VX
+                break;
+
+                case 0x001E:    // FX1E: Adds VX to I
+                break;
+
+                case 0x0029:    // FX29: Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font
+                break;
+
+                case 0x0033:    // FX33: Stores the Binary-coded decimal representation of VX, with 
+                                //       the most significant of three digits at the address in I, 
+                                //       the middle digit at I plus 1, 
+                                //       and the least significant digit at I plus 2
+                break;
+
+                case 0x0055:    // FX55: Stores V0 to VX in memory starting at address I
+                break;
+
+                case 0x0065:    // FX65: Fills V0 to VX with values from memory starting at address I
+                break;
+
+                default:
+                    console.log("Unknown opcode: " + opcode.toString(16));
+            }
+        break;
+
+        default:
+            console.log("Unknown opcode: " + opcode.toString(16));
+    }
+}
+
 function emulateCycle(){
     //Fetch opcode, instructions are 2 bytes long
     opcode = memory[pc] << 8 | memory[pc+1];
-    //Decode opcode
-    //Execute opcode
+    //Decode and execute opcode
 
     //Update timers
 }
