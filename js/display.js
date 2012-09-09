73487289d2fcb4b73660207cf13e57cc3ca11a6b
Colin Kuebler <kueblc@rpi.edu>
2012-09-09 15:54:52 -0400

    Unified Display class to contain both Canvas and Div implementations

--- js/display.js
+++ js/display.js
@@ -1,29 +1,36 @@
 /* TO USE:
 // create the display object
-var display = new DivDisplay( 64, 32, 12 )
+var display = new Display( 64, 32, 12, !CanvasRenderingContext2D )
 // clear screen
 display.fill(0).flush();
 // add to the DOM
 document.getElementById('screen').appendChild( display.container );
 */
 
-function DivDisplay( width, height, pixelSize ){
+function Display( width, height, pixelSize, polyfill ){
 	/* CONSTRUCTOR */
 	var api = this,
 		length = width * height,
-		container = document.createElement('div'),
-		pixel = document.createElement('div'),
-		pixels = container.childNodes,
 		buffer = [],
 		colorMap = {
 			0: '#000',
 			1: '#FFF' };
-	container.style.width = width * pixelSize + 'px';
-	container.style.height = height * pixelSize + 'px';
-	pixel.style.width = pixel.style.height = pixelSize + 'px';
-	pixel.style.float = 'left';
-	for( var i = 0; i < length; i++ )
-		container.appendChild( pixel.cloneNode() );
+	if( polyfill ){
+		var container = document.createElement('div'),
+			pixel = document.createElement('div'),
+			pixels = container.childNodes;
+		container.style.width = width * pixelSize + 'px';
+		container.style.height = height * pixelSize + 'px';
+		pixel.style.width = pixel.style.height = pixelSize + 'px';
+		pixel.style.float = 'left';
+		for( var i = 0; i < length; i++ )
+			container.appendChild( pixel.cloneNode() );
+	} else {
+		var container = document.createElement('canvas'),
+			context = container.getContext('2d');
+		container.width = width * pixelSize;
+		container.height = height * pixelSize;
+	}
 	/* PUBLIC VARIABLES AND FUNCTIONS */
 	api.colorMap = colorMap;
 	api.container = container;
@@ -32,52 +39,23 @@ function DivDisplay( width, height, pixelSize ){
 			buffer[i] = value;
 		return api;
 	};
-	api.flush = function(){
-		for( var i = 0; i < length; i++ )
-			pixels[i].style.backgroundColor = colorMap[ buffer[i] ];
-		return api;
-	};
-	api.setPixel = function( x, y, value ){
-		buffer[ y * width + x ] = value;
-		return api;
-	};
-	api.getPixel = function( x, y ){
-		return buffer[ y * width + x ];
-	};
-	return api;
-};
-
-function CanvasDisplay( width, height, pixelSize ){
-	/* CONSTRUCTOR */
-	var api = this,
-		length = width * height,
-		container = document.createElement('canvas'),
-		context = container.getContext('2d'),
-		buffer = [],
-		colorMap = {
-			0: '#000',
-			1: '#FFF' };
-	container.width = width * pixelSize;
-	container.height = height * pixelSize;
-	/* PUBLIC VARIABLES AND FUNCTIONS */
-	api.colorMap = colorMap;
-	api.container = container;
-	api.fill = function( value ){
-		for( var i = 0; i < length; i++ )
-			buffer[i] = value;
-		return api;
-	};
-	api.flush = function(){
-		for( var y = 0, i = 0; y < height; y++ ){
-			for( var x = 0; x < width; x++, i++ ){
-				context.fillStyle = colorMap[ buffer[i] ];
-				context.fillRect(
-					x * pixelSize, y * pixelSize,
-					pixelSize, pixelSize );
+	api.flush = polyfill ?
+		function(){
+			for( var i = 0; i < length; i++ )
+				pixels[i].style.backgroundColor = colorMap[ buffer[i] ];
+			return api;
+		} :
+		function(){
+			for( var y = 0, i = 0; y < height; y++ ){
+				for( var x = 0; x < width; x++, i++ ){
+					context.fillStyle = colorMap[ buffer[i] ];
+					context.fillRect(
+						x * pixelSize, y * pixelSize,
+						pixelSize, pixelSize );
+				}
 			}
-		}
-		return api;
-	};
+			return api;
+		};
 	api.setPixel = function( x, y, value ){
 		buffer[ y * width + x ] = value;
 		return api;
